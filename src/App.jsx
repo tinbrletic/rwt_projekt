74d3396c2e7a72824967b586a6d43ba1c77977b1
tinbrletic <tin.pingvin@gmail.com>
2023-11-15 18:35:54 +0100

    Importanje fetch funkcije

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Stanje za pohranu podataka iz backend-a
+  const [data, setData] = useState(null);
+  // Stanje za pohranu eventualnih grešaka prilikom dohvaćanja podataka
+  const [error, setError] = useState(null);
+
+  // Funkcija za dohvaćanje podataka s backend-a
+  const fetchDataFromBackend = async () => {
+    try {
+      // Slanje HTTP GET zahtjeva na backend endpoint
+      const response = await fetch('URL'); // Zamijeni URL s odgovarajućim URL-om tvog backend servera
+
+      // Provjera status koda odgovora
+      if (response.ok) {
+        // Ako je odgovor uspješan, parsiraj JSON odgovor
+        const result = await response.json();
+        // Postavi podatke u stanje
+        setData(result);
+      } else {
+        // Ako odgovor nije uspješan, postavi grešku u stanje
+        setError('Nešto je pošlo po zlu prilikom dohvaćanja podataka.');
+      }
+    } catch (error) {
+      // Uhvati i postavi grešku u stanje u slučaju bilo kakve druge greške
+      setError('Greška prilikom obrade zahtjeva:', error);
+    }
+  };
 
   return (
     <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={fetchDataFromBackend()}>
           Klinkni me!
         </button>
       </div>
+
+      {/* Prikaz podataka ako su dostupni */}
+      {data && (
+        <div className="data-container">
+          {/* Ovdje prikaži podatke kako želiš */}
+          <p>{data.exampleProperty}</p>
+        </div>
+      )}
+
+      {/* Prikaz greške ako se dogodi */}
+      {error && <p className="error-message">{error}</p>}
     </>
   )
 }
