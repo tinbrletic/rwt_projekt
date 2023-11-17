be6a2e39ac6cb6e7f7e2af3d9621d8c5feb5913d
tinbrletic <tin.pingvin@gmail.com>
2023-11-17 18:19:40 +0100

    napokon radi gumb

--- Projekt0/src/main/java/com/example/Projekt0/MyController.java
+++ Projekt0/src/main/java/com/example/Projekt0/MyController.java
@@ -1,15 +1,17 @@
 package com.example.Projekt0;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173") // Replace with your React frontend URL
 public class MyController {
-	
-	@GetMapping("/greet")
-	public String greet() {
-		return "hey";
-	}
+
+    @GetMapping("/hey")
+    public String getHey() {
+        return "hey";
+    }
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,30 @@
-import { useState } from 'react'
-import Axios from 'axios';
-import './App.css'
+// YourReactComponent.js
 
-function App() {
-  // Stanje za pohranu podataka iz backend-a
-  const [data, setData] = useState(null);
-  // Stanje za pohranu eventualnih grešaka prilikom dohvaćanja podataka
-  const [error, setError] = useState(null);
+import React, { useState } from 'react';
 
-  // Funkcija za dohvaćanje podataka s backend-a
-  const fetchDataFromBackend = async () => {
-    try {
-      // Slanje HTTP GET zahtjeva na backend endpoint
-      const response = await fetch('URL'); // Zamijeni URL s odgovarajućim URL-om tvog backend servera
+const YourReactComponent = () => {
+    const [buttonColor, setButtonColor] = useState('red');
 
-      // Provjera status koda odgovora
-      if (response.ok) {
-        // Ako je odgovor uspješan, parsiraj JSON odgovor
-        const result = await response.json();
-        // Postavi podatke u stanje
-        setData(result);
-      } else {
-        // Ako odgovor nije uspješan, postavi grešku u stanje
-        setError('Nešto je pošlo po zlu prilikom dohvaćanja podataka.');
-      }
-    } catch (error) {
-      // Uhvati i postavi grešku u stanje u slučaju bilo kakve druge greške
-      setError('Greška prilikom obrade zahtjeva:', error);
-    }
-  };
+    const handleButtonClick = () => {
+        fetch('http://localhost:8080/api/hey') // Change the URL to match your Spring Boot server
+            .then(response => response.text())
+            .then(data => {
+                if (data === 'hey') {
+                    setButtonColor('green');
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    };
 
-  return (
-    <>
-      <div className="card">
-        <button onClick={fetchDataFromBackend()}>
-          Klinkni me!
-        </button>
-      </div>
-
-      {/* Prikaz podataka ako su dostupni */}
-      {data && (
-        <div className="data-container">
-          {/* Ovdje prikaži podatke kako želiš */}
-          <p>{data.exampleProperty}</p>
+    return (
+        <div>
+            <button style={{ backgroundColor: buttonColor }} onClick={handleButtonClick}>
+                Click me
+            </button>
         </div>
-      )}
+    );
+};
 
-      {/* Prikaz greške ako se dogodi */}
-      {error && <p className="error-message">{error}</p>}
-    </>
-  )
-}
-
-export default App
+export default YourReactComponent;
