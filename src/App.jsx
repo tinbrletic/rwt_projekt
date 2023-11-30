0354af75aad3a7d6c6e389069da8d7012f1f1f5a
tinbrletic <tin.pingvin@gmail.com>
2023-11-30 22:35:13 +0100

    forma

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,80 @@ import React, { useState } from 'react';
 
 const YourReactComponent = () => {
     const [buttonColor, setButtonColor] = useState('red');
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+    });
 
-    const handleButtonClick = () => {
-        fetch('http://localhost:8080/api/hey') // Change the URL to match your Spring Boot server
-            .then(response => response.text())
-            .then(data => {
-                if (data === 'hey') {
-                    setButtonColor('green');
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = () => {
+        // Send data to the backend (replace with your backend URL)
+        fetch('http://localhost:8080/api/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            // Handle success response from the backend
+
+            // Assuming the backend responds with 'hey'
+            if (data === 'hey') {
+                setButtonColor('green');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Handle error response from the backend
+        });
     };
 
     return (
         <div>
+            <form>
+                {/* Input for a username */}
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleInputChange}
+                    required
+                />
+                
+                {/* Input for a password */}
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    required
+                />
+
+                {/* Button to submit the form */}
+                <button type="button" onClick={handleFormSubmit}>
+                    Submit
+                </button>
+            </form>
+            
+            {/* Button that changes color based on the response 
             <button style={{ backgroundColor: buttonColor }} onClick={handleButtonClick}>
                 Click me
-            </button>
+            </button>*/}
+            
         </div>
     );
 };
