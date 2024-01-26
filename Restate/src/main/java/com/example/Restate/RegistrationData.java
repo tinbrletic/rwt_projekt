f5c4d02881411a3c83bee9a01b28bd411f3a92df
Bruno Bezjak <123963426+Bruno0211@users.noreply.github.com>
2024-01-26 12:24:39 +0100

    Add files via upload

---
+++ Restate/src/main/java/com/example/Restate/RegistrationData.java
@@ -0,0 +1,33 @@
+package com.example.Restate;
+
+public class RegistrationData {
+	private String username;
+	private String password;
+	
+	public RegistrationData() {
+        // Default constructor required for JSON deserialization
+    }
+
+    public RegistrationData(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
