90cd330a35bad3ae9cf1ee50e4ac882c05b4c511
Bruno Bezjak <123963426+Bruno0211@users.noreply.github.com>
2024-02-02 00:27:12 +0100

    Add files via upload

---
+++ RealEstate/RealEstate/src/main/java/com/example/RealEstate/HashedData.java
@@ -0,0 +1,39 @@
+package com.example.RealEstate;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "hashedDataCollection")
+public class HashedData {
+	
+    @Id
+    private String id;
+    private String hashedData;
+
+    // Constructors, getters, setters (required by MongoDB)
+
+    public HashedData() {
+        // Default constructor required by MongoDB
+    }
+
+    public HashedData(String hashedData) {
+        this.hashedData = hashedData;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getHashedData() {
+        return hashedData;
+    }
+
+    public void setHashedData(String hashedData) {
+        this.hashedData = hashedData;
+    }
+    
+}
