26e4b16ccd56b200eea7b53d027d739a84375f25
Bruno Bezjak <123963426+Bruno0211@users.noreply.github.com>
2024-02-01 23:10:34 +0100

    Add files via upload

---
+++ Restate/src/main/java/com/example/Restate/RealEstateListing.java
@@ -0,0 +1,68 @@
+package com.example.Restate;
+
+public class RealEstateListing {
+    private String street;
+    private int sqMeters;
+    private int floors;
+    private int numOfRooms;
+    private int numOfBathrooms;
+    private int yearOfConstruction;
+    private int numOfResidents;
+    private boolean hasParking;
+    private String pictureUrl;
+    
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public int getSqMeters() {
+		return sqMeters;
+	}
+	public void setSqMeters(int sqMeters) {
+		this.sqMeters = sqMeters;
+	}
+	public int getFloors() {
+		return floors;
+	}
+	public void setFloors(int floors) {
+		this.floors = floors;
+	}
+	public int getNumOfRooms() {
+		return numOfRooms;
+	}
+	public void setNumOfRooms(int numOfRooms) {
+		this.numOfRooms = numOfRooms;
+	}
+	public int getNumOfBathrooms() {
+		return numOfBathrooms;
+	}
+	public void setNumOfBathrooms(int numOfBathrooms) {
+		this.numOfBathrooms = numOfBathrooms;
+	}
+	public int getYearOfConstruction() {
+		return yearOfConstruction;
+	}
+	public void setYearOfConstruction(int yearOfConstruction) {
+		this.yearOfConstruction = yearOfConstruction;
+	}
+	public int getNumOfResidents() {
+		return numOfResidents;
+	}
+	public void setNumOfResidents(int numOfResidents) {
+		this.numOfResidents = numOfResidents;
+	}
+	public boolean isHasParking() {
+		return hasParking;
+	}
+	public void setHasParking(boolean hasParking) {
+		this.hasParking = hasParking;
+	}
+	public String getPictureUrl() {
+		return pictureUrl;
+	}
+	public void setPictureUrl(String pictureUrl) {
+		this.pictureUrl = pictureUrl;
+	}
+}
